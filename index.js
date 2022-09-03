4dad37f9c723198ada47443bc9ff9a35775676e9
nigarkhan12 <nigarkhan92@gmail.com>
2022-09-03 21:51:15 +0530

    Installed EJS and Rendered Template

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const bicycles = require('./data/data.json');
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    return res.send(bicycles)
+    return res.render('bicycles');
 });
 
 
@@ -12,7 +14,7 @@ app.get('/bicycle', (req, res) => {
     console.log(req.query.id);
     const bicycle = bicycles.find(b => b.id === req.query.id);
 
-    return res.send(bicycle);
+    return res.render('overview');
 })
 
 app.listen(3000, () => console.log('Server is running at Port 3000'));
