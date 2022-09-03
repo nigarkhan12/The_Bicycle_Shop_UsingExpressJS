eac2ec015d0f545a248989515e65e65341cbf13c
nigarkhan12 <nigarkhan92@gmail.com>
2022-09-03 21:36:29 +0530

    declaring all the routes

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => { 
-    return res.send('Hello NIgar')
+const bicycles = require('./data/data.json');
+
+app.get('/', (req, res) => {
+    return res.send(bicycles)
+});
+
+
+app.get('/bicycle', (req, res) => { 
+    console.log(req.query.id);
+    const bicycle = bicycles.find(b => b.id === req.query.id);
+
+    return res.send(bicycle);
 })
 
 app.listen(3000, () => console.log('Server is running at Port 3000'));
